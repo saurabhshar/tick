163c2d9f51dfbc8c8268ab6f4c94ed1f18547d27
sausharm1 <sausharm1@publicisgroupe.net>
2021-07-26 03:21:32 +0530

    test

--- src/main/java/com/sol/service/impl/TickServiceImpl.java
+++ src/main/java/com/sol/service/impl/TickServiceImpl.java
@@ -2,6 +2,7 @@ package com.sol.service.impl;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -16,15 +17,18 @@ import com.sol.utils.Constants;
 import com.sol.vo.Stats;
 import com.sol.vo.Tick;
 import com.sol.work.SlidingWindow;
+import com.sol.work.StatsWorker;
 
 @Service
 public class TickServiceImpl implements TickService {
 
 	private final ConcurrentSkipListMap<Long, Double> allTickPrices = new ConcurrentSkipListMap<>();
-	private final ScheduledExecutorService allStatsCalcScheduler = Executors.newScheduledThreadPool(1);
 	private final AtomicReference<Stats> allStatsRef = new AtomicReference<>();
 
 	private final ConcurrentHashMap<String, SlidingWindow> instrumentsWindows = new ConcurrentHashMap<>();
+
+	private final ScheduledExecutorService allStatsCalcScheduler = Executors.newScheduledThreadPool(1);
+	private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
 	public TickServiceImpl() {
 		StatsCalc calculator = new StatsCalculator(allTickPrices, allStatsRef, Constants.COLLISION_SIZE);
@@ -32,37 +36,8 @@ public class TickServiceImpl implements TickService {
 	}
 
 	public void add(Tick tick) {
-		addWithUniqueTickTime(tick.getTimestamp(), tick.getPrice());
-		
-		String instrument = tick.getInstrument();
-		SlidingWindow window = null;
-		if (!instrumentsWindows.contains(instrument)) {
-			window = new SlidingWindow();
-			window.addToQueue(tick);
-		}
-		instrumentsWindows.merge(instrument, window, (w, t) -> w.addToQueue(tick));
-		
-	}
-
-	/*
-	 * Takes care of collision, and always generates a unique key for map - This is
-	 * only for all ticks, for individual instrument this is not
-	 * possible(assumption - per second tick generation, but could be implemented if
-	 * generation is fast)
-	 */
-	private long addWithUniqueTickTime(long timestamp, double price) {
-		long index = timestamp * Constants.COLLISION_SIZE;
-		long maxblock = index + Constants.COLLISION_SIZE - 1;
-		while (true) {
-			Double val = allTickPrices.putIfAbsent(index, price);
-			if (val == null || index > maxblock)
-				break;
-			else {
-				index = index + 1;
-			}
-		}
-
-		return index;
+		StatsWorker worker = new StatsWorker(tick, instrumentsWindows, allTickPrices);
+		executor.submit(worker);
 	}
 
 	public Stats getStats(String instrument) {
---
+++ src/main/java/com/sol/work/StatsWorker.java
@@ -0,0 +1,56 @@
+package com.sol.work;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import com.sol.utils.Constants;
+import com.sol.vo.Tick;
+
+public class StatsWorker implements Runnable {
+
+	private final Tick tick;
+	private final ConcurrentHashMap<String, SlidingWindow> instrumentsWindows;
+	private final ConcurrentSkipListMap<Long, Double> allTickPrices;
+
+	public StatsWorker(Tick tick, ConcurrentHashMap<String, SlidingWindow> instrumentsWindows,
+			ConcurrentSkipListMap<Long, Double> allTickPrices) {
+		this.tick = tick;
+		this.instrumentsWindows = instrumentsWindows;
+		this.allTickPrices = allTickPrices;
+	}
+
+	@Override
+	public void run() {
+		addForAllWithUniqueTick(tick.getTimestamp(), tick.getPrice());
+		addForSingleInstrument(tick);
+	}
+
+	private void addForSingleInstrument(Tick tick) {
+		String instrument = tick.getInstrument();
+		SlidingWindow window = null;
+		if (!instrumentsWindows.containsKey(instrument)) {
+			window = new SlidingWindow();
+			window.addToQueue(tick);
+		}
+		instrumentsWindows.merge(instrument, window, (w, t) -> w.addToQueue(tick));
+	}
+
+	/*
+	 * Takes care of collision, and always generates a unique key for map - This is
+	 * only for all ticks, for individual instrument this is not possible(assumption
+	 * - per second tick generation, but could be implemented if generation is fast)
+	 */
+	private void addForAllWithUniqueTick(long timestamp, double price) {
+		long index = timestamp * Constants.COLLISION_SIZE;
+		long maxblock = index + Constants.COLLISION_SIZE - 1;
+		while (true) {
+			Double val = allTickPrices.putIfAbsent(index, price);
+			if (val == null || index > maxblock)
+				break;
+			else {
+				index = index + 1;
+			}
+		}
+	}
+
+}
